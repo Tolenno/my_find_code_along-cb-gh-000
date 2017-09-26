82969cd7c30efa455a8232db7041564e6005ad67
Matt Hough <matt@hough.tv>
2017-09-26 12:07:32 +0000

    Done.

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -4,17 +4,7 @@ def my_find(collection)
   i = 0
   return_array = []
   while i < collection.size
-    if yield(collection[i])
-      return collection[i]
-    end
+    return collection[i] if yield(collection[i])
     i += 1
   end
 end
-
-#1. i = 0
-#2. return_array = []
-#3. while i < collection.size
-#4. yield(collection[i])
-#5. i += 1
-#6. search to find array elements that the block evaluates true and store in return_array
-#7. return return_array
